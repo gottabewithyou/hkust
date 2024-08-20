667e11aa090e9a7505f28c4df7ea7f064b4ac7b3
LongKit <143519689+gottabewithyou@users.noreply.github.com>
2024-08-20 16:22:54 +0800

    Edited script.sh

--- script.sh
+++ script.sh
@@ -2,13 +2,8 @@
 
 while true; do
   nc chal.firebird.sh 33004 << EOF
-a
+a\n
 EOF
-  read -r -d '' response <&1
-  if [[ "$response" =~ "Unfortunate. Try again" ]]; then
-    echo "Trying again..."
-  else
-    echo "Success!"
-    break
-  fi
+  read -r -d '' response <<< "$(cat)"  # Use a here string to capture the entire output
+  echo "$response"
 done
