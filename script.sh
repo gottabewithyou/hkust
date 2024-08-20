52fb99a64632193468bcbecb2de1bdd5d43d98eb
LongKit <143519689+gottabewithyou@users.noreply.github.com>
2024-08-20 16:30:19 +0800

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true; do
-  nc chal.firebird.sh 33004 << EOF
-a\n
-EOF
-  read -r -d '' response <<< "$(cat)"  # Use a here string to capture the entire output
-  echo "$response"
-done
